eabc4c3d6be6249a52e07f2b2bb19683e84697bd
adri <adrian.g.job@gmail.com>
2025-12-29 16:16:58 +0100

    removed stupid comments

--- src/features/TaskList/hooks/useTaskForm.jsx
+++ src/features/TaskList/hooks/useTaskForm.jsx
@@ -1,12 +1,11 @@
-// hooks/useTaskForm.js
 import { useForm } from 'react-hook-form';
 
 function useTaskForm() {
   const {
-    register,           // Para conectar los inputs
-    handleSubmit,       // Para manejar el envío
-    reset,              // Para limpiar el formulario
-    formState: { errors } // Para errores
+    register,           
+    handleSubmit,       
+    reset,              
+    formState: { errors } 
   } = useForm();
 
   return {
