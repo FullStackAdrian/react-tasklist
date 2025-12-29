eb839c603a3668047de9cc4f54b7fd3e6710a366
adri <adrian.g.job@gmail.com>
2025-12-29 16:16:50 +0100

    added TaskList hook

---
+++ src/features/TaskList/hooks/useTaskList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useState } from "react";
+
+export const useTaskList = (data) => {
+      const [tasks, setTasks] = useState();
+    const parseData = (data) => {};
+    const onAddTask = (data) => {};
+}
