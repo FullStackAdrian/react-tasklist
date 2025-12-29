03545d7baeb95e05b3b4addfd3d3c94806e94c10
adri <adrian.g.job@gmail.com>
2025-12-29 17:17:43 +0100

    removed controlers directory, and formated tasklistcomponent

--- src/features/TaskList/components/TaskListComponent.jsx
+++ src/features/TaskList/components/TaskListComponent.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import TaskList from "../ui/TaskList.jsx";
+import useTaskList from "../hooks/useTaskList.jsx";
 
-export function TaskListComponent() {
+function TaskListComponent() {
+    const { tasks } = useTaskList();
 
+    return <TaskList tasks={tasks} />;
 }
+
+export default TaskListComponent;
