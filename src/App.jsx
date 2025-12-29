bb04178296380477632312a8cc98dc3b91b445c4
adri <adrian.g.job@gmail.com>
2025-12-29 17:55:14 +0100

    added TaskListComponent to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
-import TaskFormComponent from './features/TaskList/components/TaskFormComponent.jsx'
+import "./App.css";
+import TaskFormComponent from "./features/TaskList/components/TaskFormComponent.jsx";
+import TaskListComponent from "./features/TaskList/components/TaskListComponent.jsx";
+
 function App() {
-  return (
-    <>
-    <TaskFormComponent></TaskFormComponent>
-    </>
-  )
+    const { tasks } = useTaskList();
+    return (
+        <>
+            <TaskFormComponent />
+            <TaskListComponent />
+        </>
+    );
 }
 
-export default App
+export default App;
