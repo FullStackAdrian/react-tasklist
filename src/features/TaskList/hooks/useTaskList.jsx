6bb043cd80b2a8b5b463fa1bb2705289085f72bd
adri <adrian.g.job@gmail.com>
2026-01-01 23:26:54 +0100

    sorting tasklist by eisenhower box

--- src/features/TaskList/hooks/useTaskList.jsx
+++ src/features/TaskList/hooks/useTaskList.jsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 
 function useTaskList() {
     const [tasks, setTasks] = useState([]);
+
+    // sort recived tasks( data ) by Eisenhower Box
+    // “What is important is seldom urgent and what is urgent is seldom important.”
+    const sortTasks = (list) => [...list].sort((a, b) => b.important * 2 + b.urgent - (a.important * 2 + a.urgent));
+
     const onAddTask = (data) => {
-        setTasks((prev) => [...prev, data]);
+        setTasks((prev) => sortTasks([...prev, data]));
     };
 
     return { onAddTask, tasks };
