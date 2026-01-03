256e0787f5e19db8d23b00ba1cb5107460def806
adri <adrian.g.job@gmail.com>
2026-01-03 18:19:49 +0100

    removed more stuff from css and added mainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import MainLayout from "./layouts/mainLayout.jsx";
 import TaskListPage from "./features/TaskList/pages/taskListPage.jsx";
 
 function App() {
-
     return (
         <>
-         <TaskListPage/>
+            <MainLayout>
+                <TaskListPage />
+            </MainLayout>
         </>
     );
 }
--- src/features/TaskList/pages/taskListPage.jsx
+++ src/features/TaskList/pages/taskListPage.jsx
@@ -4,25 +4,18 @@ import FilterBarFormComponent from "../components/filterBarFormComponent.jsx";
 import useTaskList from "../hooks/useTaskList.jsx";
 
 function TaskListPage() {
-    const { tasks, onAddTask, applyFilter : onFilter  } = useTaskList();
+    const { tasks, onAddTask, applyFilter: onFilter } = useTaskList();
 
     return (
-        <div className="min-h-screen bg-slate-900 p-6">
-            <div className="max-w-7xl mx-auto">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    
-                    {/* Columna 1: Formulario */}
-                    <div>
-                        <TaskFormComponent onAddTask={onAddTask} />
-                    </div>
-                    
-                    {/* Columna 2: Filtro + Lista (contenedor unificado) */}
-                    <div className="bg-slate-800 rounded-lg border border-slate-700 p-6">
-                        <h2 className="text-xl font-semibold text-slate-100 mb-6">Task List</h2>
-                        <FilterBarFormComponent onFilter={onFilter} />
-                        <TaskListComponent tasks={tasks} />
-                    </div>
-                    
+        <div className=" overflow-hidden p-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 overflow-auto">
+                <div>
+                    <TaskFormComponent onAddTask={onAddTask} />
+                </div>
+                <div className="bg-slate-800 rounded-lg border border-slate-700 p-6">
+                    <h2 className="text-xl font-semibold text-slate-100 mb-6">Task List</h2>
+                    <FilterBarFormComponent onFilter={onFilter} />
+                    <TaskListComponent tasks={tasks} />
                 </div>
             </div>
         </div>
